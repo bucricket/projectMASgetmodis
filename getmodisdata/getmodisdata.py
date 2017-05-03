e30b56b75a9c56acab745295ff720c745947f786
bucricket <bucricket@gmail.com>
2017-05-03 16:44:15 -0400

    Fixed string error

--- getmodisdata/getmodisdata.py
+++ getmodisdata/getmodisdata.py
@@ -62,11 +62,11 @@ if not os.path.exists(landsatTemp):
 def getMODISdata(tiles,product,version,startDate,endDate,auth):    
 
     if version.startswith('MCD'):
-        folder = "MOTA"
+        folder = 'MOTA'
     elif version.startswith('MOD'):
-        folder = "MOLT"
+        folder = 'MOLT'
     else:
-        folder = "MOLA"
+        folder = 'MOLA'
     subprocess.call(["modis_download.py", "-r", "-U", "%s" % auth[0], "-P", 
                     "%s" % auth[1],"-p", "%s.%s" % (product,version), "-t", 
                     "%s" % tiles,"-s","%s" % folder, "-f", "%s" % startDate,"-e", "%s" % endDate, 
